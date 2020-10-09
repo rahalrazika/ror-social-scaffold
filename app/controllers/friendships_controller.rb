b4c00e76e6cf1e738899075f65bd2d8474e795cb
rahalrazika <rahalraika@gmail.com>
2020-10-09 15:35:04 +0200

    fix rubocop errors

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,24 +1,22 @@
 class FriendshipsController < ApplicationController
-    def create 
+  def create
     @friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
     if @friendship.save
-        flash[:notice] = "You are now friends"
+      flash[:notice] = 'You are now friends'
     else
-        flash[:alert] = "Friendship error, please try again"
+      flash[:alert] = 'Friendship error, please try again'
     end
     redirect_to root_path
-    end
+  end
 
+  def destroy
+    Friendship.find(params[:id]).destroy
+    redirect_to root_path
+  end
 
-    def destroy
-        Friendship.find(params[:id]).destroy
-        redirect_to root_path 
+  private
 
-    end 
-
-    private
-    def friendship_params
-        params.require(:frienship).permit(:confirmed)
-
-    end
+  def friendship_params
+    params.require(:frienship).permit(:confirmed)
+  end
 end
