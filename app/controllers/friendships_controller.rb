d0ce513f73b34b12c2fbad12874888eb9b362437
rahalrazika <rahalraika@gmail.com>
2020-10-09 15:28:40 +0200

    add the add friend button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,22 +1,24 @@
 class FriendshipsController < ApplicationController
-  def create
-    @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
+    def create 
+    @friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
     if @friendship.save
-      flash[:notice] = 'You are now friends'
+        flash[:notice] = "You are now friends"
     else
-      flash[:alert] = 'Friendship error, please try again'
+        flash[:alert] = "Friendship error, please try again"
     end
     redirect_to root_path
-  end
+    end
 
-  def destroy
-    Friendship.find(params[:id]).destroy
-    redirect_to root_path
-  end
 
-  private
+    def destroy
+        Friendship.find(params[:id]).destroy
+        redirect_to root_path 
 
-  def friendship_params
-    params.require(:frienship).permit(:confirmed)
-  end
+    end 
+
+    private
+    def friendship_params
+        params.require(:frienship).permit(:confirmed)
+
+    end
 end
