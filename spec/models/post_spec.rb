22a1a9393f9778e3760b9aca9b8fb3643ac7505b
rahalrazika <rahalraika@gmail.com>
2020-10-13 16:33:50 +0200

    add the post spec

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+    before {@post = FactoryBot.build(:post)}
+
+    it 'Validates presence of content' do
+        expect(Post.new).to validate_presence_of(:content)
+    end
+
+    it { should belong_to(:user) }
+    it { should have_many(:likes) }
+    it { should have_many(:comments) }
+
+end
