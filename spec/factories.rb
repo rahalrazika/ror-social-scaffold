eef15c206255e292550a1d733042809f608b948d
jacobrees <jacob.rees97@yahoo.com>
2020-10-13 16:02:45 +0100

    Correct rubocop linter errors

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,19 +3,22 @@ FactoryBot.define do
     name { 'test' }
     email { 'aabb@hh.de' }
     password { 'ruby123' }
-    id {1}
+    id { 1 }
   end
 
   factory :post do
-  user_id {1}
-  content {'Lorem Ipsum is simply dummy text of the printing and typesetting industry
-  Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
-  when an unknown printer took a galley of type and scrambled it to '}
+    user_id { 1 }
+    content do
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry
+  Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+  when an unknown printer took a galley of type and scrambled it to '
+    end
   end
 
   factory :comment do
-    content {'Lorem Ipsum is simply dummy text of the printing and typesetting industry
-    Lore '}
+    content do
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry
+    Lore '
     end
+  end
 end
-
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-    it { should belong_to(:user) }
-    it { should belong_to(:friend) }
-    it {should allow_value(true).for(:confirmed)}
-    it {should allow_value(false).for(:confirmed)}
+  it { should belong_to(:user) }
+  it { should belong_to(:friend) }
+  it { should allow_value(true).for(:confirmed) }
+  it { should allow_value(false).for(:confirmed) }
 end
