b4bcb1d0f776a7f723708f592ba4c24c42097f9e
jacobrees <jacob.rees97@yahoo.com>
2020-10-19 13:43:32 +0100

    Remove all logic from users/index.html.erb and users/show.html.erb, place this logic inside of application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,7 +29,7 @@ module ApplicationHelper
   end
 
   def friend?(user)
-    current_user.friendships.where(friend_id: user.id, user_id: current_user.id, confirmed: false).count.positive?
+    current_user.friendships.where(friend_id: user.id, user_id: current_user.id, confirmed: true).count.positive?
   end
 
   def friend_reverse?(user)
@@ -42,5 +42,61 @@ module ApplicationHelper
 
   def post_belong_to_user?(post)
     Friendship.where(user_id: post.user.id, confirmed: true).count.positive?
+  end
+
+  def friend_requests(user)
+    user_count = user.friendships.where(friend_id: current_user.id, user_id: user.id, confirmed: false).count
+    html = ''
+    if user_count.positive?
+      html << "<li> Friend Request: #{user.name} </li>"
+      html << button_to('Accept Friend Request', friendship_path(user), method: :put)
+      html << button_to('Decline Request', friendship_path(current_user.id), method: :delete)
+    end
+    html.html_safe
+  end
+
+  def users_list(user)
+    html = ''
+    unless current_user_is_user?(user)
+      html << "<li> Name: #{user.name}</li>"
+      if request_sent?(user)
+        html << '<p>Friendship Request Pending</p>'
+        html << button_to('Cancel Request', friendship_path(user.id), method: :delete)
+      elsif friend?(user)
+        html << button_to('Unfriend', friendship_path(user.id), method: :delete)
+      elsif friend_reverse?(user)
+        html << button_to('Unfriend', friendship_path(current_user.id), method: :delete)
+      elsif request_sent?(user)
+        html << button_to('Decline Request', friendship_path(current_user.id), method: :delete)
+        html << button_to('Accept Friend Request', friendship_path(user), method: :put)
+      else
+        html << button_to('Add Friend', friendships_path, params: { friend_id: user.id })
+      end
+      html << "<span class='profile-link'>"
+      html << link_to('See Profile', user_path(user), class: 'profile-link')
+      html << '</span>'
+    end
+    html.html_safe
+  end
+
+  def user_show_friendship_button(user)
+    html = ''
+    html << "<h2> Name: #{user.name}</h2>"
+    unless current_user_is_user?(user)
+      if request_sent?(user)
+        html << '<p>Friendship Request Pending</p>'
+        html << button_to('Cancel Request', friendship_path(user.id), method: :delete)
+      elsif friend?(user)
+        html << button_to('Unfriend', friendship_path(user.id), method: :delete)
+      elsif friend_reverse?(user)
+        html << button_to('Unfriend', friendship_path(current_user.id), method: :delete)
+      elsif request_sent?(user)
+        html << button_to('Decline Request', friendship_path(current_user.id), method: :delete)
+        html << button_to('Accept Friend Request', friendship_path(user), method: :put)
+      else
+        html << button_to('Add Friend', friendships_path, params: { friend_id: user.id })
+      end
+    end
+    html.html_safe
   end
 end
